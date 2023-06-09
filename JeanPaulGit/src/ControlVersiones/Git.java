ebdd200aae7bfce3b92dcb5c220ec13db12d0a3b
Yanyo <jeanp.matgal@educa.jcyl.es>
2023-06-09 19:03:06 +0200

    mi segundo commit

--- JeanPaulGit/src/ControlVersiones/Git.java
+++ JeanPaulGit/src/ControlVersiones/Git.java
@@ -3,6 +3,7 @@ package ControlVersiones;
 public class Git {
 	public static void main(String[] args) {
 		System.out.println("Empezando Proyecto");
+		System.out.println("Realizando Modificacion en Git");
 		
 	}
 }
